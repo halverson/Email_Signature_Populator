eda6ef159bee190545d830296884862958838b74
Brent Nygaard <nygaardmeister@gmail.com>
2017-12-19 21:50:06 +0200

    begin javascript functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-// this is where things get real
+function addNameItem () {
+  var node = document.createElement('LI');
+  var content = document.createTextNode('<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName" /><button type="button" name="addName" id="addNameBtn">+</button>');
+  node.appendChild(content);
+  document.getElementById('addNameList').appendChild(node);
+}
+
+var addBTN = document.getElementById('addNameBtn');
+
+addBTN.onclick = addNameItem();
