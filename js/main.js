2ed549e43320aaa8b0d29afa2ee703759be700c1
Brent Nygaard <nygaardmeister@gmail.com>
2017-12-20 16:41:40 +0200

    More js playtime

--- js/main.js
+++ js/main.js
@@ -5,8 +5,29 @@
   document.getElementById('addNameList').appendChild(node);
 } */
 
+function createHTMLelem (htmlStr) {
+  var frag = document.createDocumentFragment();
+  var temp = document.createElement('li');
+  temp.innerHTML = htmlStr;
+  while (temp.firstChild) {
+    frag.appendChild(temp.firstChild);
+  }
+  return frag;
+}
+
 var addBTN = document.getElementById('addNameBtn');
-// addBTN.onclick = addNameItem();
+
+var fragment = createHTMLelem('<li id="addNameItem"><input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName" /> <button type="button" name="addName" id="addNameBtn">+</button></li>');
+
 addBTN.onclick = function () {
-  addBTN.style.backgroundColor = 'red';
+  console.log('Click!');
+  document.getElementById('addNameList').appendChild(fragment);
 };
+
+/* addBTN.onclick = function () {
+  var list = document.getElementById('addNameList');
+  var node = document.createElement('LI');
+  var content = document.createTextNode('<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName" /><button type="button" name="addName" id="addNameBtn">+</button>');
+  node.appendChild(content);
+  list.appendChild(node);
+}; */
