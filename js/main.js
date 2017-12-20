88a4a83c423228173c4553edcbf553ccfe01bd4d
Brent Nygaard <nygaardmeister@gmail.com>
2017-12-20 10:02:52 +0200

    play with js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-function addNameItem () {
+/* function addNameItem () {
   var node = document.createElement('LI');
   var content = document.createTextNode('<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName" /><button type="button" name="addName" id="addNameBtn">+</button>');
   node.appendChild(content);
   document.getElementById('addNameList').appendChild(node);
-}
+} */
 
 var addBTN = document.getElementById('addNameBtn');
-
-addBTN.onclick = addNameItem();
+// addBTN.onclick = addNameItem();
+addBTN.onclick = function () {
+  addBTN.style.backgroundColor = 'red';
+};
