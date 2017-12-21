27bb320051f1220efe81a094c3b657add89dd511
Brent Nygaard <nygaardmeister@gmail.com>
2017-12-21 14:56:09 +0200

    add basic generate functionality

--- js/main.js
+++ js/main.js
@@ -6,6 +6,33 @@ document.addEventListener('click', function (e) {
 
 function addLI () {
   var node = document.createElement('LI');
-  node.innerHTML = '<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName"><button type="button" name="addName" class="addNameBtn">+</button>';
+  node.innerHTML = '<input type="text" name="fullName" value="" placeholder="Name Surname" class="fullName"><button type="button" name="addName" class="addNameBtn">+</button>';
   document.getElementById('addNameList').appendChild(node);
 }
+
+function getNames (elem) {
+  var namesList = document.getElementsByClassName(elem);
+  var namesFinal = [];
+  for (var i = 0; i < namesList.length; i++) {
+    namesFinal[i] = namesList[i].value;
+  }
+  return namesFinal;
+}
+
+function getTemplate (elem) {
+  var input = document.getElementById(elem);
+  if (input.value !== '') {
+    return input.value;
+  } else {
+    return '';
+  }
+}
+
+function generate () {
+  var names = getNames('fullName');
+  var path = getTemplate('templatePath');
+  console.log(names);
+  console.log('File path: ' + path);
+}
+
+document.getElementById('genBtn').addEventListener('click', generate);
