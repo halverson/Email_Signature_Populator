76e552ef740a5ab169f4f3b3d3a521e25552c31e
Brent Nygaard <nygaardmeister@gmail.com>
2017-12-21 11:56:30 +0200

    fix event listener on plus button

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
-var addBTN = document.getElementsByClassName('addNameBtn');
+document.addEventListener('click', function (e) {
+  if (e.target.className === 'addNameBtn') {
+    addLI();
+  }
+});
 
-for (var i = 0; i < addBTN.length; i++) {
-  addBTN[i].addEventListener('click', function () {
-    var node = document.createElement('LI');
-    node.setAttribute('id', 'addNameItem');
-    node.innerHTML = '<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName"> <button type="button" name="addName" class="addNameBtn">+</button>';
-    document.getElementById('addNameList').appendChild(node);
-  });
-  console.log(addBTN);
-}
-
-/* addBTN.addEventListener('click', function () {
+function addLI () {
   var node = document.createElement('LI');
-  node.setAttribute('id', 'addNameItem');
-  node.innerHTML = '<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName"> <button type="button" name="addName" class="addNameBtn">+</button>';
+  node.innerHTML = '<input type="text" name="fullName" value="" placeholder="Name Surname" id="fullName"><button type="button" name="addName" class="addNameBtn">+</button>';
   document.getElementById('addNameList').appendChild(node);
-}); */
+}
